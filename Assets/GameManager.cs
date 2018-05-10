72b01c513f8bfb290b5d0bc6c3e5bef578eceeb3
Brett Sheleski <brett@sheleski.com>
2018-05-10 11:12:14 -0500

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public float moveTimer = 0.5f,
+                 turnDelay = 0.25f;
+
+    public PlayerController player;
+
+    public KeyCode turnLeft = KeyCode.LeftArrow, 
+        turnRight = KeyCode.RightArrow, 
+        turnDown = KeyCode.DownArrow, 
+        turnUp = KeyCode.UpArrow;
+
+    private float lastMoveTime;
+
+	// Use this for initialization
+	void Start () {
+        lastMoveTime = Time.time;
+        lastTurnTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        TurnPlayer();
+
+        MovePlayer();
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GrowPlayer();
+        }
+	}
+
+    private void GrowPlayer()
+    {
+        if (player != null)
+        {
+            player.Grow();
+        }
+    }
+
+    float lastTurnTime;
+    private void TurnPlayer()
+    {
+        float now = Time.time;
+        if (player != null && (now - lastTurnTime) >= turnDelay)
+        {
+            Vector3 turn = Vector3.zero;
+
+            if (Input.GetKey(turnLeft))
+            {
+                turn += Vector3.left;
+            }
+
+            if (Input.GetKey(turnRight))
+            {
+                turn += Vector3.right;
+            }
+
+            if (Input.GetKey(turnDown))
+            {
+                turn += Vector3.down;
+            }
+
+            if (Input.GetKey(turnUp))
+            {
+                turn += Vector3.up;
+            }
+
+            if (turn != Vector3.zero)
+            {
+                lastTurnTime = now;
+
+                player.Turn(turn);
+            }
+        }
+        
+    }
+
+    private void MovePlayer()
+    {
+        if (player != null)
+        {
+            float now = Time.time;
+
+            if (now - lastMoveTime >= moveTimer)
+            {
+                player.MoveForward();
+                lastMoveTime = now;
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public GameObject head, tail;
+    public GameObject bodyPrefab;
+
+    private List<GameObject> bodyPieces = new List<GameObject>();
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveForward()
+    {
+        Vector3 position = head.transform.position;
+        Vector3 nextPosition;
+        foreach(var bodyPart in bodyPieces)
+        {
+            nextPosition = bodyPart.transform.position;
+
+            bodyPart.transform.position = position;
+
+            position = nextPosition;
+        }
+
+        tail.transform.position = position;
+
+        head.transform.Translate(Vector3.forward);
+    }
+
+    public void TurnLeft()
+    {
+        Turn(Vector3.left);
+    }
+
+    public void TurnRight()
+    {
+        Turn(Vector3.right);
+    }
+
+    public void TurnUp()
+    {
+        Turn(Vector3.up);
+    }
+
+    public void TurnDown()
+    {
+        Turn(Vector3.down);
+    }
+
+    public void Turn(Vector3 direction)
+    {
+        // left/right --> rotation over y axis
+        // up/down --> rotation over x axis
+        Vector3 r = new Vector3(direction.y * -90, direction.x * 90);
+
+        head.transform.Rotate(r);
+
+        //head.transform.Rotate(direction);
+    }
+
+    public void Grow(int count)
+    {
+        GameObject newPiece;
+        Vector3 position = tail.transform.position;
+        for(int i = 0; i < count; ++i)
+        {
+            newPiece = GameObject.Instantiate(bodyPrefab, position, this.transform.rotation, this.gameObject.transform);
+            bodyPieces.Add(newPiece);
+            position += Vector3.back;
+        }
+
+        tail.transform.position = position;
+    }
+
+    public void Grow()
+    {
+        Grow(1);
+    }
+}
